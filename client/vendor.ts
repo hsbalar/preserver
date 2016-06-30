6dfcb27fbfb2b7a6d8dd9a91bdd64565592ec266
Hitesh Balar <hsbalar@gmail.com>
2016-06-30 22:51:51 +0530

    Optimise application source

--- client/vendor.ts
+++ client/vendor.ts
@@ -1,7 +1,7 @@
 // Prefer CoreJS over the polyfills above
 import 'core-js/es6';
 import 'core-js/es7/reflect';
-require('zone.js/dist/zone');
+import 'zone.js/dist/zone';
 
 // Typescript emit helpers polyfill
 import 'ts-helpers';
@@ -11,10 +11,5 @@ import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
 import '@angular/http';
-import '@angular/router';
-
-// RxJS
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
 
 import './assets/vendor/fontawesome.scss';
--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
-import methodOverride from "method-override";
-import cookieParser from "cookie-parser";
-import favicon from "serve-favicon";
-
-import fs from "fs";
 import path from "path";
 
 import routes from "../routes";
@@ -15,55 +9,11 @@ const app = express();
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'jade');
 
-// pretty output
-if (app.get('env') === 'development') {
-  app.locals.pretty = true;
-}
-
 // serve static content from 'public' dir
-app.use(favicon(path.resolve('public/favicon.ico')));
 app.use(express.static(path.resolve('public')));
 app.use(express.static(path.resolve('dist')));
 
-// register body parsers
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// allow method override
-app.use(methodOverride());
-
 // register routes
 routes(app);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 export default app;
